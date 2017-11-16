d291850ee5e7c5f355ee6f821544834a82a296d3
gianmatteo.spoto <gianmatteo.spoto@gaming1.com>
2017-11-16 11:15:20 +0100

    add ennemy event

--- src/modules/players/players.gateway.ts
+++ src/modules/players/players.gateway.ts
@@ -1,3 +1,4 @@
+import { EnemyEvent } from './../enemies/enemies.enum';
 import { Player } from './players.model';
 import { PlayerService } from './players.service';
 import { SubscribeMessage, WebSocketGateway, WebSocketServer, OnGatewayConnection } from '@nestjs/websockets';
@@ -15,29 +16,41 @@ export class PlayersGateway {
     public async handleDisconnectPlayer(client){
         console.log('Player has disconnected: ' + client.id);
         this.playerService.delete(client.id);
-        client.broadcast.emit(PlayerEvent.RemovePlayer, {id: client.id});
+        client.broadcast.emit(PlayerEvent.Remove, {id: client.id});
     }
 
-    @SubscribeMessage(PlayerEvent.NewPlayer)
+    @SubscribeMessage(PlayerEvent.New)
     public handleNewPlayer(client, data){
         console.log('new player: ' + client.id);
         this.playerService.create(new Player(client.id, data.color));
-        client.broadcast.emit(PlayerEvent.NewPlayer, {id: client.id, color: data.color});
+        client.broadcast.emit(PlayerEvent.New, {id: client.id, color: data.color});
         for(let player  of this.playerService.players.values()) {
-            client.emit(PlayerEvent.NewPlayer, {id: player.getID(), color: player.getColor()});
+            client.emit(PlayerEvent.New, {id: player.getID(), color: player.getColor()});
         }
     }
 
-    @SubscribeMessage(PlayerEvent.MovePlayer)
+    @SubscribeMessage(PlayerEvent.Move)
     public async handleMovePlayer(client, data){
-        client.emit(PlayerEvent.MovePlayer, {id: client.id, direction: data.direction});
-        client.broadcast.emit(PlayerEvent.MovePlayer, {id: client.id, direction: data.direction});
+        client.emit(PlayerEvent.Move, {id: client.id, direction: data.direction});
+        client.broadcast.emit(PlayerEvent.Move, {id: client.id, direction: data.direction});
     }
 
-    @SubscribeMessage(PlayerEvent.StopPlayer)
+    @SubscribeMessage(PlayerEvent.Stop)
     public async handleStopPlayer(client, data){
-        client.emit(PlayerEvent.StopPlayer, {id: client.id});
-        client.broadcast.emit(PlayerEvent.StopPlayer, {id: client.id});
+        client.emit(PlayerEvent.Stop, {id: client.id});
+        client.broadcast.emit(PlayerEvent.Stop, {id: client.id});
     
+    }
+
+    @SubscribeMessage(EnemyEvent.Move)
+    public async handleMoveEnemy(client, data){
+        client.emit(EnemyEvent.Move, {index: data.index, direction: data.direction});
+        client.broadcast.emit(EnemyEvent.Move, {index: data.index, direction: data.direction});
+    }
+
+    @SubscribeMessage(EnemyEvent.Respawn)
+    public async handleRemoveEnemy(client, data){
+        client.emit(EnemyEvent.Respawn, {index: data.index});
+        client.broadcast.emit(EnemyEvent.Respawn, {index: data.index});
     }
 }
